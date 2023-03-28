c0300b256e7e2230ee2e8ee4dba390846042271a
appuser <appuser@journey2-95cdcd64d-kjvlj>
2023-03-28 11:51:57 +0000

    starter project

---
+++ client/public/script.js
@@ -0,0 +1,14 @@
+const element = function (tag, inner) {
+  return `<${tag}>${inner}</${tag}>`;
+} 
+
+const loadEvent = function() {
+
+  // Write your JavaScript code after this line
+  
+
+  // Write your JavaScript code before this line
+
+}
+
+window.addEventListener("load", loadEvent);
